926beaf2008dcb17f753309fb84eed84da23c818
Ben Liu <benliu.1997@gmail.com>
2017-09-23 11:38:44 -0400

    fixed login page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,8 +13,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( three.min.js )
-
-
-Rails.application.config.assets.precompile += %w( path.scss )
-Rails.application.config.assets.precompile += %w( main.scss )
-Rails.application.config.assets.precompile += %w( login.scss )
